eaa1fd515e03fb25298deab721599c666e1f891a
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-03-25 11:25:36 +0100

    feat(Ex1ComplexNumber.scala): refactor Complex type to be a tuple of (Double, Double) for better representation feat(Ex1ComplexNumber.scala): implement methods re(), im(), sum(), subtract(), and asString() for Complex type using pattern matching for clarity and correctness

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -22,11 +22,21 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = (Double,Double)
+    def complex(re: Double, im: Double): Complex = (re:Double, im:Double)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case (re:Double, im:Double) => re
+      def im(): Double = complex match
+        case (re:Double, im:Double) => im
+      def sum(other: Complex): Complex =  complex match
+        case (re:Double, im:Double) => (other.re() + complex.re(), other.im() + complex.im())
+
+      def subtract(other: Complex): Complex = complex match
+        case (re:Double, im:Double) => (complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String = complex match
+        case (re:Double, im:Double) =>  val str = re.toString + " + " + im.toString + "i"
+          if re.equals(0.0) || im.equals(0.0)
+          then str.replace(" + 0.0i", "").replace("0.0 + ", "")
+          else str.replace(" + -", " - ")
+
